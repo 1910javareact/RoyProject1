a26a4877b41d4f023467e2e201459c2232a8d3c3
Joshua Roy <joshua.roy@revature.net>
2019-12-13 13:51:24 -0500

    beginning of conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
 import './App.css'
-class App extends Component {
-  constructor (props: Readonly<{}>) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick () {
-    console.log('Success!')
-  }
-  render () {
-    return (
-      <div className='button__container'>
-        <button className='button' onClick={this.handleClick}>
-          Login
-        </button>
-      </div>
-    )
-  }
+import { Provider } from 'react-redux';
+import { Router, Switch, Route } from 'react-router';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Router>
+          <nav>
+            <ERSNav />
+          </nav>
+          <Switch>
+            <Route path='/login' component={LoginComponent} />
+            <Route path='/reimbursement' component={ReimbursementComponent} />
+            <Route path='/user' component={UserComponent} />
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
+    </div>
+
+  );
 }
-export default App
+
+export default App;
